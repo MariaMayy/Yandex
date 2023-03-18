5b4f07a681060d9edc314880790b6883a6b7566f
Maria <64471671+MariaMayy@users.noreply.github.com>
2023-03-19 00:19:44 +0300

    Add task 25

---
+++ DP_1/25_Carnations/25_Carnations.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> dp(N);
+    vector<int> vCur(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> vCur[i];
+    }
+
+    sort(vCur.begin(), vCur.end());
+
+    dp[1] = vCur[1] - vCur[0];
+    dp[2] = vCur[2] - vCur[0];
+
+    for (int i = 3; i < N; i++) {
+        dp[i] = min(dp[i - 1], dp[i - 2]) + vCur[i] - vCur[i - 1]; // 1. связали оптимально (i-2) гвоздика и связали i с i-1
+        // 2. либо i-2 уже соединен с i-1, то есть (i-1) оптимально соединены и надо еще связать i с i-1
+    }
+
+    cout << dp[N - 1];
+    return 0;
+}
